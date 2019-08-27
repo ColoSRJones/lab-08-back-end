f6646bbd177368c6e7f72d05a81464840851c3cc
ColoSRJones <colosrjones@gmail.com>
2019-08-26 20:21:59 -0500

    fixed

--- schema.sql
+++ schema.sql
@@ -1,28 +1,29 @@
-DROP TABLE IF EXISTS locations, weather, events, yelps, movies, trails CASCADE;
+DROP TABLE IF EXISTS locations, weather, events, yelps, movies;
 
 CREATE TABLE locations (
   id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
-  latitude NUMERIC(20, 14),
-  longitude NUMERIC(20, 14),
-  created_at BIGINT
+  latitude NUMERIC(10, 7),
+  longitude NUMERIC(10, 7)
 );
 
 CREATE TABLE weather (
   id SERIAL PRIMARY KEY,
-  forecast VARCHAR(255),
-  time VARCHAR (255),
   location_id INTEGER NOT NULL REFERENCES locations(id),
+  forecast VARCHAR(255),
+  time VARCHAR(255),
   created_at BIGINT
+
 );
 
 CREATE TABLE events (
   id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  link VARCHAR(255),
   name VARCHAR(255),
   event_date VARCHAR(255),
-  link VARCHAR(255),
-  summary VARCHAR(10000),
+  summary VARCHAR,
   location_id INTEGER NOT NULL REFERENCES locations(id),
   created_at BIGINT
 );
@@ -30,39 +31,23 @@ CREATE TABLE events (
 CREATE TABLE yelps (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  image_url VARCHAR(1000),
+  image_url VARCHAR(255),
   price CHAR(5),
-  rating NUMERIC(2, 1),
-  url VARCHAR(1000),
-  location_id INTEGER NOT NULL REFERENCES locations(id),
-  created_at BIGINT
+  rating NUMERIC(2,1),
+  url VARCHAR(255),
+  created_at BIGINT,
+  location_id INTEGER NOT NULL REFERENCES locations(id)
 );
 
 CREATE TABLE movies (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
-  overview VARCHAR(5000),
-  average_votes NUMERIC(8, 4),
+  overview VARCHAR(1000),
+  average_votes NUMERIC(4,2),
   total_votes INTEGER,
-  image_url VARCHAR(1000),
-  popularity NUMERIC(8, 4),
+  image_url VARCHAR(255),
+  popularity NUMERIC(6,4),
   released_on CHAR(10),
-  query VARCHAR(255) NOT NULL,
-  created_at BIGINT
-);
-
-CREATE TABLE trails (
-  id SERIAL PRIMARY KEY,
   created_at BIGINT,
-  name VARCHAR(255),
-  location VARCHAR(255),
-  length FLOAT,
-  stars FLOAT,
-  star_votes INTEGER,
-  summary VARCHAR(10000),
-  trail_url VARCHAR(1000),
-  conditions VARCHAR(500),
-  condition_date VARCHAR(10),
-  condition_time VARCHAR(10),
   location_id INTEGER NOT NULL REFERENCES locations(id)
 );
